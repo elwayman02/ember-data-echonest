1d2bb4778c4cd4e25de6bacac265d3681462fe20
Jordan Hawker <hawker.jordan@gmail.com>
2015-09-12 23:42:29 -0700

    Upgrade package dependencies to latest version

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -18,6 +18,16 @@ module.exports = function(environment) {
       // when it is created
     },
 
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self'",
+      'font-src': "'self'",
+      'connect-src': "'self' http://developer.echonest.com",
+      'img-src': "'self'",
+      'style-src': "'self'",
+      'media-src': "'self'"
+    }
+
     ECHONEST_KEY: process.env.ECHONEST_KEY
   };
 
