ed20f6e1a9c37fe1524aa79fb8009a3131cdf68d
Jordan Hawker <hawker.jordan@gmail.com>
2015-10-27 18:11:04 -0700

    Refactor serializer logic to be inherited

--- addon/serializers/echonest-genre.js
+++ addon/serializers/echonest-genre.js
@@ -1,20 +1,5 @@
-import Ember from 'ember';
-import DS from 'ember-data';
+import EchonestSerializer from 'ember-data-echonest/serializers/echonest';
 
-const { isPresent } = Ember;
-const { RESTSerializer } = DS;
-
-export default RESTSerializer.extend({
-    normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-        if (isPresent(payload.response) && isPresent(payload.response.genres)) {
-            const genres = payload.response.genres.map(function (genre, index) {
-                genre.id = index;
-                return genre;
-            });
-
-            return this._super(store, primaryModelClass, { 'echonest-genre': genres }, id, requestType);
-        }
-
-        return this._super.apply(this, arguments);
-    }
+export default EchonestSerializer.extend({
+    modelKey: 'genre'
 });
