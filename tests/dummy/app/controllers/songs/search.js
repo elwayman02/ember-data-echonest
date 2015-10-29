d8b7081a74db4be0b63507ae0eddd75f6340e97d
Jordan Hawker <hawker.jordan@gmail.com>
2015-10-28 18:52:44 -0700

    Allow selection of search by field in song demo

--- tests/dummy/app/controllers/songs/search.js
+++ tests/dummy/app/controllers/songs/search.js
@@ -2,17 +2,32 @@ import Ember from 'ember';
 
 const { Controller, isPresent } = Ember;
 
+const searchTypes = [{
+    key: 'combined',
+    label: 'Title/Artist'
+}, {
+    key: 'title',
+    label: 'Title'
+}, {
+    key: 'artist',
+    label: 'Artist'
+}];
+
 export default Controller.extend({
+    searchTypes,
+    searchKey: searchTypes[0],
+
     genres: [],
 
     actions: {
-        search(title) {
-            title = title || this.get('title');
-            if (isPresent(title)) {
+        search(keyword) {
+            keyword = keyword || this.get('keyword');
+            if (isPresent(keyword)) {
                 this.store.query('echonest-song', {
-                    title,
+                    [this.get('searchKey.key')]: keyword,
                     method: 'search',
                     bucket: ['song_currency', 'song_discovery', 'song_hotttnesss', 'song_type'],
+                    sort: 'song_hotttnesss-desc',
                     results: 100
                 }).then((songs) => {
                     this.set('songs', songs);
